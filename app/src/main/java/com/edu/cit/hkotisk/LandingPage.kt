dd2298fe58f2cb03cb0aa18a2da392d5d3b0bb48
Sting421 <aldrinjohnvitorillo0229@gmail.com>
2025-03-25 10:25:51 +0800

    first commit

---
+++ app/src/main/java/com/edu/cit/hkotisk/LandingPage.kt
@@ -0,0 +1,11 @@
+package com.edu.cit.hkotisk
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LandingPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing_page)
+    }
+}
